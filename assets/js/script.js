9ac4e673999a3d31e656b2e4c541e45028a35b39
Spencer Merrill <canadianfaller7@gmail.com>
2022-10-26 00:26:47 -0600

    added some code on how to get my current and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ const forecastSearch = async search => {
         weatherQuery = `https://api.openweathermap.org/data/2.5/onecall?lat=${result[0].lat}&lon=${result[0].lon}&appID=${appID}&units=imperial`;
     })
     .catch(err => {
-        window.alert('Could not find a city with that name! Please try again')
+        alert('Could not find a city with that name! Please try again')
     })
     // if the information is correct 
     if(weatherQuery){
@@ -25,5 +25,52 @@ const forecastSearch = async search => {
         })
         .catch(err => console.log(err));
     }
+}
+
+// getting the information for current day forcast based on the info that got parsed earlier in the forecastSearch function
+const todayForecast = async (data, city) => {
+    const current = data.current;
+    const date = new Date(current.dt * 1000).toLocaleDateString('en-US');
+    const getIcon = current.weather[0].icon;
+    const icon = `https://openweathermap.org/img/wn/${getIcon}.png`;
+
+    // making html elements to add the info for our weather
+    const currentHtml = `<h1 class=cap-letter>${city} (${date}) <img src='${icon}'></h1>
+                        <p>Temp: ${Math.floor(current.temp)}°F</p>
+                        <p>Humidity: ${current.humidity}%</p>
+                        <p>Wind Speed: ${current.wind_speed} MPH</p>`;
+
+    $('#today-content').html('');
+    // showing our todays forecast block
+    $('#todays-forecast').css("display", "block")
+    // appending currentHtml to display on screen
+    $('#today-content').append(currentHtml);
+    // adding the city searched to our local storage
+    addToSearchHistory(city);
+}
+
+// doing the same thing as above, but for 5 days
+const fiveDayForecast = (data, city) => {
+    const daily = data.daily;
+
+    $('#forecast-container').html('');
+
+    daily.forEach((day, index) => {
+        if(index < 5){
+
+            const i = daily[index];
+            const iIcon = i.weather[0].icon;
+            const icon = `http://openweathermap.org/img/wn/${iIcon}.png`;
+            const date = new Date(i.dt * 1000).toLocaleDateString('en-US');
+            const fiveDayWeather = `<div class='day-forecast'>
+                                <h3>${date}</h3>
+                                <img src='${icon}'></img>
+                                <p>Temperature: ${Math.floor(i.temp.day)}°F</p>
+                                <p>Wind Speed: ${i.wind_speed} MPH</p>
+                                <p>Humidity: ${i.humidity}%</p>
+                            </div>`;
+            $('#forecast-container').append(fiveDayWeather);
+        }
+    });
 }
 
