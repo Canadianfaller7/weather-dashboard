159cba8cfe64b64a679865bbefb4e2615a24254e
Spencer Merrill <82860126+Canadianfaller7@users.noreply.github.com>
2022-11-23 23:17:28 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ const fiveDayForecast = (data, city) => {
     $('#forecast-container').html('');
 
     daily.forEach((day, index) => {
-        if(index < 5){
+        if(index < 8){
             const i = daily[index];
             const iIcon = i.weather[0].icon;
             const icon = `http://openweathermap.org/img/wn/${iIcon}.png`;
